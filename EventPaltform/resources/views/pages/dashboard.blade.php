4d4654bab0bfca384a72b37aa18bc6a2e16271a2
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-02-29 17:53:52 +0100

    new style for buttons and add new buttons for edit and delete events

--- EventPaltform/resources/views/pages/dashboard.blade.php
+++ EventPaltform/resources/views/pages/dashboard.blade.php
@@ -3,20 +3,34 @@
 @section('content')
 <h1 class="text-center">Events {{count($events)}}</h1>
 
-<div class="container">
+<div class="container text-center">
     <!-- link to get create page -->
-<a href="{{route('users.create')}}">Click here to add a new event</a>
-    <div class="row">
+@auth
+    <button class="btn btn-primary m-3"><a class="text-white" href="{{route('users.create')}}">Click here to add a new event</a></button>   
+@endauth
+    <div class="row text-start">
         @foreach ($events as $event)
-            <div class="col-sm-12 col-md-6 col-xl-3 p-3">
+            <div class="col-sm-12 col-md-6 col-xl-3 p-2">
                 <div class="container border higth">
                     <a class="anchor_index" href="{{route('users.show', $event->id)}}">
-                        <h5>{{$event->name}}</h5>
-                        <h5>{{$event->location}}</h5>
-                        <span>Description: </span><p>
+                        <h5><strong>Name: </strong>{{$event->name}}</h5>
+                        <h5><strong>Where: </strong>{{$event->location}}</h5>
+                        <span><strong>Description: </strong> </span><p>
                             {{$event->description}}
                         </p>
                     </a>
+                    @auth
+                        <div class="row">
+                            <div class="col">
+                                <button class="btn btn-primary"><a class="text-white" href=""><i class="fa-solid fa-pencil"></i> Edit</a></button>
+                            </div>
+                            <div class="col">
+                                <form action="">
+                                    <input class="btn btn-danger" type="submit" value="Delete">
+                                </form>
+                            </div>
+                        </div>
+                    @endauth
                 </div>
             </div>
         @endforeach

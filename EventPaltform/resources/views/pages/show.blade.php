fd13a5537afe9ef281093ac100bf1e342c6cd591
Cristopher Ferrari <ferraricristopher93@gmail.com>
2024-02-29 17:19:21 +0100

    Create show page

--- EventPaltform/resources/views/pages/show.blade.php
+++ EventPaltform/resources/views/pages/show.blade.php
@@ -1,6 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<h1 class="text-center">Show event: {{$event->id}}</h1>
-
+<h1 class="text-center my-4">Show event: {{$event->id}}</h1>
+<div class="container border p-3">
+    <div class="text-center">
+        <h1><strong>Event name: </strong>{{$event->name}}</h1>
+        <ul>
+            <li><strong>Event's date: </strong>{{$event->date}}</li>
+            <li><strong>Description: </strong>{{$event->description}}</li>
+            <li><strong>Location: </strong>{{$event->location}}</li>
+        </ul>
+        <ul>
+            @foreach ($event->tags as $tag)
+                <li>#{{$tag->name}}</li>
+            @endforeach
+        </ul>
+    </div>
+</div>
 @endsection
